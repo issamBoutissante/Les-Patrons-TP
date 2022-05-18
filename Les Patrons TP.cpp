92ce01641850b225495556c4904eb2c229689f98
Boutissatne Issam <boutissante.issam.dev@gmail.com>
2022-05-18 17:07:42 +0200

    Patrons de fonctions Termine

--- Les Patrons TP.cpp
+++ Les Patrons TP.cpp
@@ -24,12 +24,44 @@ void afficheElements(vector<T> tab,int taille) {
 }
 #pragma endregion
 
+#pragma region Somme des elements de tableau
+template<typename T>
+T getSomme(vector<T> tab) {
+    T somme = 0;
+    for (int i = 0; i < tab.size(); i++)
+        somme += tab[i];
+    return somme;
+}
+#pragma endregion
+
+#pragma region Max valeur de tableau
+template<typename T>
+T getMax(vector<T> tab) {
+    T max = 0;
+    for (int i = 0; i < tab.size(); i++)
+        if (max < tab[i])
+            max = tab[i];
+    return max;
+}
+#pragma endregion
+
 
 int main()
 {
+    //Test getCarre(T nombre)
+    cout << "- Le carre de 5.5 est : " << getCarre<double>(5.5) << endl;
+
+    //Test getMax(T a,T b)
+    cout << "- Le nombre max entre 10 et 10.5 est : " << getMax<double>(10, 10.5) << endl;
+
     //Test afficheElements(vector<T> tab,int taille)
     vector<double> notes = { 19,18,19.5,20,20 };
     afficheElements<double>(notes,notes.size());
 
+    //Test getSomme(vector<T> tab)
+    cout<<"- La somme des notes est : "<<getSomme<double>(notes)<<endl;
+
+    //Test getMax(vector<T> tab)
+    cout << "- La note max est : " << getMax<double>(notes) << endl;
     return 200;
 }
